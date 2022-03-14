22e74b6198ff1cb7ef2bc393c280165b4a0a6ef7
J_Wawrzyniak <100858953+Jnotfound@users.noreply.github.com>
2022-03-14 08:12:03 +0100

    Update string.php

--- 3Gi1T/string.php
+++ 3Gi1T/string.php
@@ -25,5 +25,19 @@ $text="jaNUsz koWAlski";
 echo "<hr>$text<br>";
 echo mb_strtolower ($text)."<br>";
 echo ucfirst(mb_strtolower ($text))."<br>";
-echo ucwords(mb_strtolower ($text))."<br>";
+echo ucwords(mb_strtolower ($text))."<br><hr>";
+
+$lorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br>";
+echo $lorem."<hr>";
+echo wordwrap($lorem, 30, "<br>");
+echo wordwrap($lorem, 30, "<hr>");
+ob_clean();
+
+$name ="  Jan  ";
+echo "Długość imienia $name wynosi:".strlen($name)."<br>";
+echo "Długość imienia ".ltrim($name)." wynosi:".strlen(ltrim($name))."<br>";
+echo "Długość imienia ".rtrim($name)." wynosi:".strlen(rtrim($name))."<br>";
+echo "Długość imienia ".trim($name)." wynosi:".strlen(trim($name))."<br>";
+
+
 ?>
