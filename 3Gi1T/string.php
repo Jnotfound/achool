1d4ae2b5284aa0a9c77bfc15fca4e21abf96169b
J_Wawrzyniak <100858953+Jnotfound@users.noreply.github.com>
2022-03-14 07:46:04 +0100

    Update string.php

--- 3Gi1T/string.php
+++ 3Gi1T/string.php
@@ -8,4 +8,22 @@ SCHOOL;
 echo "$text<br>";
 echo nl2br($text);
 
+$name="jANusZ";
+echo "$name<br>";
+echo strtolower ($name);
+echo "<br>";
+echo strtoupper ($name);
+echo "<br>";
+
+$surname="Gwóźdź";
+echo "$surname<br>";
+echo mb_strtolower ($surname);
+echo "<br>";
+echo mb_strtoupper ($surname);
+
+$text="jaNUsz koWAlski";
+echo "<hr>$text<br>";
+echo mb_strtolower ($text)."<br>";
+echo ucfirst(mb_strtolower ($text))."<br>";
+echo ucwords(mb_strtolower ($text))."<br>";
 ?>
